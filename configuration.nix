7addf4d826bd1282d6f6381985f2e7cdeb092abd
jonwin1 <jonatan.wincent@proton.me>
2024-04-12 20:50:15 +0200

    flake init

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,8 @@ in
   ];
 
   nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
     settings.auto-optimise-store = true;
     gc = {
       automatic = true;
