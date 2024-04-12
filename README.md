b0b823ad8e290bc4c50a1896d918573746190ea9
jonwin1 <jonatan.wincent@proton.me>
2024-04-12 21:10:28 +0200

    flake, home-manager

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nixos-jonwin
+
+sudo su
+nix-shell -p git
+git clone https://github.com/jonwin1/nixos-jonwin.git /mnt/etc/nixos
+nixos-install --flake .#<host>
+reboot
+...
--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -19,6 +23,13 @@
           inherit system;
           modules = [
             ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jonwin = {
+                imports = [ ./home.nix ];
+              };
+            }
           ];
         };
       };
