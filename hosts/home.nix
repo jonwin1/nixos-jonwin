b832f1175e3b5d765389a179eb2dfff5667d11e8
jonwin1 <jonatan.wincent@proton.me>
2024-04-26 09:47:48 +0200

    qutebrowser

--- hosts/home.nix
+++ hosts/home.nix
@@ -26,6 +26,20 @@
 
   programs = {
     home-manager.enable = true;
+    qutebrowser = {
+      enable = true;
+      settings = {
+        confirm_quit = [ "multiple-tabs" "downloads" ];
+        fonts.default_size = "12pt";
+        tabs.show = "multiple";
+        url = {
+          default_page = "https://searxng.jonwin.se/";
+          start_pages = "https://searxng.jonwin.se/";
+        };
+        content.autoplay = false;
+      };
+      searchEngines = { DEFAULT = "https://searxng.jonwin.se/search?q={}"; };
+    };
   };
 
   home.stateVersion = "23.11";
