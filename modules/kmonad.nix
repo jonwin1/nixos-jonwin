53d15627f227c117cd0cfb2b6be064be0c539afd
jonwin1 <jonatan.wincent@proton.me>
2024-05-02 21:09:44 +0200

    improving keymap thumpcluster

--- modules/kmonad.nix
+++ modules/kmonad.nix
@@ -38,31 +38,30 @@
               up (layer-toggle up)
               down (layer-toggle down)
 
-              elm (tap-hold-next-release 200 esc lmet)
-              sct (tap-hold-next-release 200 spc lctl)
-              ddown (tap-hold-next-release 200 del @down)
+              rdn (tap-hold-next-release 200 ret @down)
+              ddn (tap-hold-next-release 200 del @down)
             )
             (deflayer base
             =       1       2       3       4       5       6       7       8       9       0       S-/
             tab     q       w       e       r       t       y       u       i       o       p       å
-            '       a       s       d       f       g       h       j       k       l       ö       ä
-            ret     z       x       c       v       b       n       m       ,       .       -       ralt
-                            @elm    @sct    @up                     @up     bspc
-                                    lalt    @down                   @ddown
+            esc     a       s       d       f       g       h       j       k       l       ö       ä
+            '       z       x       c       v       b       n       m       ,       .       -       rctl
+                            lmet    spc     @up                     @up     bspc
+                                    lalt    @rdn                    @ddn
             )
             (deflayer up
             S-=     S-1     S-2     S-3     S-4     S-5     S-6     S-7     S-8     S-,     S-.     S-`
             S-tab   Q       W       E       R       T       Y       U       I       O       P       Å
-            S-'     A       S       D       F       G       H       J       K       L       Ö       Ä
-            S-ret   Z       X       C       V       B       N       M       ;       S-;     S--     S-ralt
-                            S-lmet  S-lctl  XX                      XX      S-bspc
-                                    S-lalt  XX                      S-del
+            S-esc   A       S       D       F       G       H       J       K       L       Ö       Ä
+            S-'     Z       X       C       V       B       N       M       ;       S-;     S--     S-rctl
+                            S-lmet  S-spc   XX                      XX      S-bspc
+                                    S-lalt  S-ret                   S-del
             )
             (deflayer down
             f12     f1      f2      f3      f4      f5      f6      f7      f8      f9      f10     f11
             tab     XX      XX      S-[     S-]     \    previoussong vold	volu nextsong   mute    caps
-            `       XX      XX      S-9     S-0     /       left    down    up      right playpause stop
-            ret     XX      XX      [       ]       S-\     home    pgdn    pgup    end     ins     ralt
+            esc     XX      XX      S-9     S-0     /       left    down    up      right playpause stop
+            `       XX      XX      [       ]       S-\     home    pgdn    pgup    end     ins     ralt
                             lmet    spc     lsft                    rsft    bspc
                                     lalt    XX                      XX
             )
