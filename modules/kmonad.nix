29b753870736ca9ec9bd1e9a86f7b949552ddf03
jonwin1 <jonatan.wincent@google.com>
2024-05-06 17:09:21 +0200

    keyboard v4.5

--- modules/kmonad.nix
+++ modules/kmonad.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, inputs, user, ... }:
+{ lib, pkgs, ... }:
 {
   environment = {
     systemPackages = with pkgs; [
@@ -35,35 +35,36 @@
                                     lalt    spc                     ralt
             )
             (defalias
-              up (layer-toggle up)
-              down (layer-toggle down)
+              sft (layer-toggle sft)
+              nav (layer-toggle nav)
 
-              rdn (tap-hold-next-release 200 ret @down)
-              ddn (tap-hold-next-release 200 del @down)
+              rtnv (tap-hold-next-release 200 ret @nav)
+              znav (tap-hold-next-release 200 z @nav)
+              -nav (tap-hold-next-release 200 - @nav)
             )
             (deflayer base
-            =       1       2       3       4       5       6       7       8       9       0       S-/
-            tab     q       w       e       r       t       y       u       i       o       p       å
-            esc     a       s       d       f       g       h       j       k       l       ö       ä
-            '       z       x       c       v       b       n       m       ,       .       -       rctl
-                            lmet    spc     @up                     @up     bspc
-                                    lalt    @rdn                    @ddn
+            =       1       2       3       4       5                       6       7       8       9       0       S-/
+            tab     q       w       e       r       t                       y       u       i       o       p       å
+            esc     a       s       d       f       g                       h       j       k       l       ö       ä
+            '       @znav   x       c       v       b                       n       m       ,       .       @-nav   del
+                                            lmet    spc     @sft    @sft    bspc
+                                                    lalt    lctl    @rtnv
             )
-            (deflayer up
-            S-=     S-1     S-2     S-3     S-4     S-5     S-6     S-7     S-8     S-,     S-.     S-`
-            S-tab   Q       W       E       R       T       Y       U       I       O       P       Å
-            S-esc   A       S       D       F       G       H       J       K       L       Ö       Ä
-            S-'     Z       X       C       V       B       N       M       ;       S-;     S--     S-rctl
-                            S-lmet  S-spc   XX                      XX      S-bspc
-                                    S-lalt  S-ret                   S-del
+            (deflayer sft
+            S-=     S-1     S-2     S-3     S-4     S-5                     S-6     S-7     S-8     S-,     S-.     S-`
+            S-tab   Q       W       E       R       T                       Y       U       I       O       P       Å
+            S-esc   A       S       D       F       G                       H       J       K       L       Ö       Ä
+            S-'     Z       X       C       V       B                       N       M       ;       S-;     S--     S-del
+                                            S-lmet  S-spc   XX      XX      S-bspc
+                                                    S-lalt  S-lctl  S-ret
             )
-            (deflayer down
-            f12     f1      f2      f3      f4      f5      f6      f7      f8      f9      f10     f11
-            tab     XX      XX      S-[     S-]     \    previoussong vold	volu nextsong   mute    caps
-            esc     XX      XX      S-9     S-0     /       left    down    up      right playpause stop
-            `       XX      XX      [       ]       S-\     home    pgdn    pgup    end     ins     ralt
-                            lmet    spc     lsft                    rsft    bspc
-                                    lalt    XX                      XX
+            (deflayer nav
+            f12     f1      f2      f3      f4      f5                      f6      f7      f8      f9      f10     f11
+            XX      XX      XX      S-[     S-]     \                    previoussong vold	volu nextsong   mute    caps
+            XX      XX      XX      S-9     S-0     /                       left    down    up      right playpause stop
+            `       XX      XX      [       ]       S-\                     home    pgdn    pgup    end     ins     ralt
+                                            XX      XX      XX      XX      XX
+                                                    XX      XX      XX
             )
           '';
         };
