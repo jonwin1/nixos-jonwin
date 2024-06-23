aaa6dfe8b4112431cafa836b0dca0b8d7dddb8be
jonwin1 <jonatan.wincent@gmail.com>
2024-06-23 17:31:42 +0200

    opengl->graphics

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -38,10 +38,9 @@
 
 
   hardware = {
-    opengl = {
+    graphics = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      enable32Bit = true;
     };
     nvidia = {
       modesetting.enable = true;
