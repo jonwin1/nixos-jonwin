43459ebc619857cfb3a8bea6b698b873ac099b81
jonwin1 <jonatan.wincent@gmail.com>
2024-06-25 10:26:44 +0200

    keyboard layout v5.0

--- modules/kmonad.nix
+++ modules/kmonad.nix
@@ -27,12 +27,12 @@
               fallthrough false
             )
             (defsrc
-            esc     f1      f2      f3      f4      f5      f6      f7      f8      f9      f10     f11
-            1       2       3       4       5       6       7       8       9       0       -       =
-            tab     q       w       e       r       t       u       i       o       p       [       ]
-            caps    a       s       d       f       g       j       k       l       ;       '       \
-                            x       c       v                       m       ,
-                                    lalt    spc                     ralt
+            esc     f1      f2      f3      f4      f5                      f6      f7      f8      f9      f10     f11
+            1       2       3       4       5       6                       7       8       9       0       -       =
+            tab     q       w       e       r       t                       u       i       o       p       [       ]
+            caps    a       s       d       f       g                       j       k       l       ;       '       \
+                            z       x       c       v       b       n       m       ,       .       /
+                                                    lalt    spc     ralt    rmet
             )
             (defalias
               sft (layer-toggle sft)
@@ -42,28 +42,28 @@
               -nav (tap-hold-next-release 200 - @nav)
             )
             (deflayer base
-            =       1       2       3       4       5                       6       7       8       9       0       S-/
+            S-=     1       2       3       4       5                       6       7       8       9       0       =
             tab     q       w       e       r       t                       y       u       i       o       p       å
             esc     a       s       d       f       g                       h       j       k       l       ö       ä
-            '       @znav   x       c       v       b                       n       m       ,       .       @-nav   del
-                                            lmet    spc     @sft    @sft    bspc
-                                                    lalt    ret     rctl
+            lctl    z       x       c       v       b                       n       m       ,       .       -       lalt
+                            left    right   @nav    spc     @sft    @sft    bspc    @nav    down    up
+                                                    lmet    ret     del     lmet
             )
             (deflayer sft
-            S-=     S-1     S-2     S-3     S-4     S-5                     S-6     S-7     S-8     S-,     S-.     S-`
+            S-/     S-1     S-2     S-3     S-4     S-5                     S-6     S-7     S-8     S-,     S-.     S-`
             S-tab   Q       W       E       R       T                       Y       U       I       O       P       Å
             S-esc   A       S       D       F       G                       H       J       K       L       Ö       Ä
-            S-'     Z       X       C       V       B                       N       M       ;       S-;     S--     S-del
-                                            S-lmet  S-spc   XX      XX      S-bspc
-                                                    S-lalt  S-ret   S-rctl
+            S-lctl  Z       X       C       V       B                       N       M       ;       S-;     S--     S-lalt
+                            S-left  S-right @nav    S-spc   XX      XX      S-bspc  @nav    S-down  S-up
+                                                    S-lmet  S-ret   S-del   S-lmet
             )
             (deflayer nav
             f12     f1      f2      f3      f4      f5                      f6      f7      f8      f9      f10     f11
-            XX      XX      XX      S-[     S-]     \                    previoussong vold	volu nextsong   mute    caps
-            XX      XX      XX      S-9     S-0     /                       left    down    up      right playpause stop
-            `       XX      XX      [       ]       S-\                     home    pgdn    pgup    end     ins     ralt
-                                            XX      XX      XX      XX      XX
-                                                    XX      XX      XX
+            XX      '       S-[     S-]     \       XX                 previoussong vold    volu nextsong   mute    caps
+            XX      S-'     S-9     S-0     /       XX                      left    down    up      right playpause stop
+            XX      `       [       ]       S-\     XX                      home    pgdn    pgup    end     ins     ralt
+                            left    right   XX      XX      XX      XX      XX      XX      down    up
+                                                    XX      XX      XX      XX
             )
           '';
         };
