794547ecef8427702a935295f4084fe6b1adb7c2
jonwin1 <jonatan.wincent@gmail.com>
2024-07-25 15:57:21 +0200

    hyprland config

--- hm-modules/hyprland.nix
+++ hm-modules/hyprland.nix
@@ -9,14 +9,20 @@
                 "$mod, W, exec, qutebrowser"
                 "$mod, S, exec, kitty"
 
+                # scratchpad
                 "$mod, P, togglespecialworkspace, magic"
                 "$mod SHIFT, P, movetoworkspace, special:magic"
 
-                "$mod, H, movefocus, l"
-                "$mod, L, movefocus, r"
-                "$mod, K, movefocus, u"
-                "$mod, J, movefocus, d"
+                "$mod, J, layoutmsg, cyclenext"
+                "$mod, K, layoutmsg, cycleprev"
+                "$mod SHIFT, J, layoutmsg, swapnext"
+                "$mod SHIFT, K, layoutmsg, swapprev"
+                "$mod, I, layoutmsg, addmaster"
+                "$mod, U, layoutmsg, removemaster"
 
+                "$mod, M, fullscreen, 0"
+                "$mod SHIFT, M, fakefullscreen, "
+                "$mod SHIFT, SPACE, togglefloating, active"
                 "$mod, Q, killactive"
                 "$mod SHIFT, Q, exit"
             ] ++ (
@@ -36,10 +42,32 @@
                 10)
             );
 
+            binde = [
+                "$mod, H, layoutmsg, mfact -0.05"
+                "$mod, L, layoutmsg, mfact +0.05"
+            ];
+
+            bindl = [
+                ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+                ", XF86AudioPrev, exec, playerctl previous"
+                ", XF86AudioNext, exec, playerctl next"
+                ", XF86AudioPause, exec, playerctl pause"
+                ", XF86AudioPlay, exec, playerctl play-pause"
+                ", XF86AudioStop, exec, playerctl stop"
+            ];
+
+            bindel = [
+                ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+                ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+                ", XF86MonBrightnessUp, exec, light -A 5"
+                ", XF86MonBrightnessDown, exec, light -U 5"
+            ];
+
             general = {
                 border_size = 2;
                 gaps_in = 0;
                 gaps_out = 0;
+                layout = "master";
             };
 
             decoration = {
@@ -64,6 +92,11 @@
                 "DP-3, 3440x1440@144, 1920x0, 1"
                 "HDMI-A-2, 1920x1080@120, 0x0, 1"
             ];
+
+            master = {
+                special_scale_factor = 0.5;
+                mfact = 0.666;
+            };
         };
     };
 }
