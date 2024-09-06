5f3a708044289d53aef374de736c18d4ad2d0824
jonwin1 <jonatan.wincent@gmail.com>
2024-09-06 13:54:55 +0200

    reduce gc interval

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -95,7 +95,7 @@ in
         gc = {
             automatic = true;
             dates = "weekly";
-            options = "--delete-older-than 14d";
+            options = "--delete-older-than 7d";
         };
     };
 
