622d198d9d9a57473f8bb27162a5607515427404
jonwin1 <jonatan.wincent@gmail.com>
2024-11-04 21:06:53 +0100

    update hdmi name

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -8,7 +8,7 @@
 
             monitor = [
                 "DP-3, 3440x1440@144, 1920x0, 1"
-                "HDMI-A-2, 1920x1080@120, 0x0, 1"
+                "HDMI-A-1, 1920x1080@120, 0x0, 1"
                 ", preferred, auto, 1"
             ];
 
@@ -18,11 +18,11 @@
                 "3, monitor:DP-3"
                 "4, monitor:DP-3"
                 "5, monitor:DP-3"
-                "6, monitor:HDMI-A-2, default:true"
-                "7, monitor:HDMI-A-2"
-                "8, monitor:HDMI-A-2"
-                "9, monitor:HDMI-A-2"
-                "10, monitor:HDMI-A-2"
+                "6, monitor:HDMI-A-1, default:true"
+                "7, monitor:HDMI-A-1"
+                "8, monitor:HDMI-A-1"
+                "9, monitor:HDMI-A-1"
+                "10, monitor:HDMI-A-1"
             ];
         };
     };
