883d99a604864ffc1a60fc0ec7cab6fda19a032d
jonwin1 <jonatan.wincent@gmail.com>
2024-11-22 20:09:26 +0100

    clean hosts default

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -167,5 +167,22 @@ in
         powerOnBoot = true;
     };
 
+    boot = {
+        loader = {
+            efi = {
+                canTouchEfiVariables = true;
+                efiSysMountPoint = "/boot";
+            };
+            grub = {
+                enable = true;
+                devices = ["nodev"];
+                efiSupport = true;
+                useOSProber = true;
+                configurationLimit = 10;
+            };
+            timeout = 5;
+        };
+    };
+
     system.stateVersion = "23.11";
 }
