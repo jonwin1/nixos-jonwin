714d1dceea46405e84b7e26ad3884f40de0254d5
jonwin1 <jonatan.wincent@gmail.com>
2025-01-06 13:13:13 +0100

    git remove global username and email

--- hm-modules/git.nix
+++ hm-modules/git.nix
@@ -2,8 +2,8 @@
     programs.git = {
         enable = true;
         lfs.enable = true;
-        userName = "jonwin1";
-        userEmail = "jonatan.wincent@gmail.com";
+        # userName = "jonwin1";
+        # userEmail = "jonatan.wincent@gmail.com";
         extraConfig = {
             init.defaultBranch = "main";
             push.autoSetupRemote = true;
