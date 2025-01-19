76bd436aee5a8893e241eb74c525563152d7ec67
jonwin1 <jonatan.wincent@proton.me>
2025-01-19 20:32:35 +0100

    nvim fix misstake

--- dotconfig/nvim/lua/plugins/color.lua
+++ dotconfig/nvim/lua/plugins/color.lua
@@ -7,10 +7,6 @@ return {
             transparent = true, -- Enable this to disable setting the background color
             terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
             styles = {
-                -- Style to be applied to different syntax groups
-                -- Value is any valid attr-list value for `:help nvim_set_hl`
-                comments = { italic = true },
-                keywords = { italic = true },
                 -- Background styles. Can be "dark", "transparent" or "normal"
                 sidebars = "dark", -- style for sidebars, see below
                 floats = "dark", -- style for floating windows
--- dotconfig/nvim/lua/plugins/treesitter.lua
+++ dotconfig/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,8 @@ return {
             ensure_installed = { "bash", "c", "cmake", "lua", "markdown", "vim", "vimdoc" },
             auto_install = true,
             -- ignore_install = { "latex" },
+            indent = { enable = true },
+            highlight = { enable = true },
         },
         config = function(_, opts)
             require("nvim-treesitter.configs").setup(opts)
