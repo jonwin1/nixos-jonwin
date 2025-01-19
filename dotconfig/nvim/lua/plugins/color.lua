5c871ef6196594360454f8273ddce85f2feb139e
jonwin1 <jonatan.wincent@gmail.com>
2025-01-19 04:17:07 +0100

    nvim add colorscheme

---
+++ dotconfig/nvim/lua/plugins/color.lua
@@ -0,0 +1,23 @@
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("tokyonight").setup({
+            transparent = true, -- Enable this to disable setting the background color
+            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+            styles = {
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+                comments = { italic = true },
+                keywords = { italic = true },
+                -- Background styles. Can be "dark", "transparent" or "normal"
+                sidebars = "dark", -- style for sidebars, see below
+                floats = "dark", -- style for floating windows
+            },
+        })
+        vim.cmd([[colorscheme tokyonight]])
+        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
+}
