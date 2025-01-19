d5ebc9466414d7014a150e55e4e0604f81b3f7e2
jonwin1 <jonatan.wincent@proton.me>
2025-01-19 20:17:31 +0100

    nvim document opts

--- dotconfig/nvim/lua/config/lazy.lua
+++ dotconfig/nvim/lua/config/lazy.lua
@@ -18,12 +18,9 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
+    -- import lua/plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
--- dotconfig/nvim/lua/config/set.lua
+++ dotconfig/nvim/lua/config/set.lua
@@ -1,10 +1,8 @@
+-- Show line number and relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.incsearch = true
-vim.opt.inccommand = "split"
-vim.opt.termguicolors = true
-
+-- Set tabs to be 4 spaces
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -13,35 +11,44 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.breakindent = true
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.cmd([[autocmd FileType tex set textwidth=80]])
-vim.cmd([[autocmd FileType md set textwidth=80]])
-
+--
 vim.opt.colorcolumn = "80"
 vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 10
+vim.g.have_nerd_font = true
+vim.opt.updatetime = 250
 
+-- Show search and replace matches while typing
+vim.opt.incsearch = true
+vim.opt.inccommand = "split"
+
+-- Ignore case for match unless explicitly using capital letters
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Automatically break lines for .tex and .md files
+vim.opt.linebreak = true
+vim.cmd([[autocmd FileType tex set textwidth=80]])
+vim.cmd([[autocmd FileType md set textwidth=80]])
+
+-- Dissable mouse
 vim.opt.mouse = ""
+
+-- Share clipboard with system
 vim.opt.clipboard = "unnamedplus"
 
+-- Configure history
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.g.have_nerd_font = true
-
-vim.opt.updatetime = 50
-vim.opt.timeoutlen = 300
-
+-- Show tabs, trailing spaces, and non-breakable spaces
+-- Also indicate when line continues outside window
 vim.opt.list = true
-vim.opt.listchars = { tab = "▏ ", trail = "·", nbsp = "␣", precedes = "«", extends = "»" }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", precedes = "«", extends = "»" }
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
