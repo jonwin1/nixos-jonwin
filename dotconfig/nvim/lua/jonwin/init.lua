a517439f1aaea297e5f49d31174a331d3f98ba4c
jonwin1 <jonatan.wincent@gmail.com>
2025-01-19 02:33:35 +0100

    nvim set

---
+++ dotconfig/nvim/lua/jonwin/init.lua
@@ -0,0 +1,1 @@
+require("jonwin.set")
---
+++ dotconfig/nvim/lua/jonwin/set.lua
@@ -0,0 +1,56 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.inccommand = "split"
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.cmd([[autocmd FileType tex set textwidth=80]])
+vim.cmd([[autocmd FileType md set textwidth=80]])
+
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+
+vim.opt.mouse = ""
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.g.have_nerd_font = false
+
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+
+vim.opt.list = true
+vim.opt.listchars = {  trail = "·", nbsp = "␣" }
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
