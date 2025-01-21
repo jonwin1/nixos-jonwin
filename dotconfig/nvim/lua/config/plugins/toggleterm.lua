86c8bd902cf960004ee132e07ca81992fb737034
jonwin1 <jonatan.wincent@proton.me>
2025-01-21 20:35:43 +0100

    nvim togglterm modifications

--- dotconfig/nvim/lua/config/plugins/toggleterm.lua
+++ dotconfig/nvim/lua/config/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-        require("toggleterm").setup{
+        require("toggleterm").setup {
 
             -- Keybind to toggle last open terminal
             open_mapping = [[<C-t>]],
@@ -26,33 +26,37 @@ return {
         local Terminal = require("toggleterm.terminal").Terminal
 
         -- Create terminals
-        local lazygit = Terminal:new({ cmd = "lazygit", dir ="git_dir", direction = "float" })
+        local lazygit = Terminal:new({ cmd = "lazygit", dir = "git_dir", direction = "float", hidden = true })
         local floating = Terminal:new({ direction = "float" })
         local horizontal = Terminal:new({ direction = "horizontal" })
         local vertical = Terminal:new({ direction = "vertical" })
 
         -- Define toggle funcitons
-        function _floating_toggle()
+        function _Floating_toggle()
             floating:toggle()
         end
-        function _lazygit_toggle()
+
+        function _Lazygit_toggle()
             lazygit:toggle()
         end
-        function _horizontal_toggle()
+
+        function _Horizontal_toggle()
             horizontal:toggle()
         end
-        function _vertical_toggle()
+
+        function _Vertical_toggle()
             vertical:toggle()
         end
 
-        -- Set keymaps
-        vim.keymap.set("n", "<leader>tf", "<cmd>lua _floating_toggle()<CR>")
-        vim.keymap.set("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>")
-        vim.keymap.set("n", "<leader>th", "<cmd>lua _horizontal_toggle()<CR>")
-        vim.keymap.set("n", "<leader>tv", "<cmd>lua _vertical_toggle()<CR>")
+        vim.keymap.set("n", "<leader>tf", "<cmd>lua _Floating_toggle()<CR>", { desc = "Terminal floating toggle" })
+        vim.keymap.set("n", "<leader>tg", "<cmd>lua _Lazygit_toggle()<CR>", { desc = "Terminal lazygit toggle" })
+        vim.keymap.set("n", "<leader>th", "<cmd>lua _Horizontal_toggle()<CR>", { desc = "Terminal horizontal toggle" })
+        vim.keymap.set("n", "<leader>tv", "<cmd>lua _Vertical_toggle()<CR>", { desc = "Terminal vertical toggle" })
+        -- Caution: terminals with different directions at the same time is not supported
+        vim.keymap.set("n", "<leader>ta", "<cmd>ToggleTermToggleAll<CR>", { desc = "Terminal all toggle" })
 
         -- Easier navigation in and out of terminals
-        vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]])
+        -- vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]])
         vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]])
         vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]])
         vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]])
