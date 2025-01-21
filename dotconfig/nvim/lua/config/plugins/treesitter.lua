f4b18c7067e0f7db090905aca1230dafdd567519
jonwin1 <jonatan.wincent@proton.me>
2025-01-21 16:12:28 +0100

    nvim treesitter no highlight large

--- dotconfig/nvim/lua/config/plugins/treesitter.lua
+++ dotconfig/nvim/lua/config/plugins/treesitter.lua
@@ -2,15 +2,27 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = { "bash", "c", "cmake", "lua", "markdown", "vim", "vimdoc" },
-            auto_install = true,
-            -- ignore_install = { "latex" },
-            indent = { enable = true },
-            highlight = { enable = true },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
+        config = function()
+            require('nvim-treesitter.configs').setup {
+
+                auto_install = true,
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown" },
+
+                highlight = {
+                    enable = true,
+
+                    -- Don't highlight in large files
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                },
+
+                indent = { enable = true },
+            }
         end,
     },
     {
