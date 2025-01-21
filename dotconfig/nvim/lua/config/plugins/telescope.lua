5c02d0d1333db4f1df13cd4ffa9467e2c1260ae4
jonwin1 <jonatan.wincent@proton.me>
2025-01-21 20:00:56 +0100

    nvim telescope keymap, leader

--- dotconfig/nvim/lua/config/plugins/telescope.lua
+++ dotconfig/nvim/lua/config/plugins/telescope.lua
@@ -22,13 +22,14 @@ return {
             require("telescope").load_extension("fzf")
 
             local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<space>sf", builtin.find_files, { desc = "Telescope find files" })
-            vim.keymap.set("n", "<space>sg", builtin.git_files, { desc = "Telescope git files" })
-            vim.keymap.set("n", "<space>sr", builtin.live_grep, { desc = "Telescope live grep" })
-            vim.keymap.set("n", "<space>sb", builtin.buffers, { desc = "Telescope buffers" })
-            vim.keymap.set("n", "<space>sh", builtin.help_tags, { desc = "Telescope help tags" })
-            vim.keymap.set("n", "<space>sm", builtin.man_pages, { desc = "Telescope man pages" })
-            vim.keymap.set("n", "<space>ss", builtin.spell_suggest, { desc = "Telescope spell suggest" })
+            vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Telescope find files" })
+            vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "Telescope git files" })
+            vim.keymap.set("n", "<leader>sr", builtin.live_grep, { desc = "Telescope live grep" })
+            vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Telescope buffers" })
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope help tags" })
+            vim.keymap.set("n", "<leader>sm", builtin.man_pages, { desc = "Telescope man pages" })
+            vim.keymap.set("n", "<leader>ss", builtin.spell_suggest, { desc = "Telescope spell suggest" })
+            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Telescope keymaps" })
         end
     }
 }
