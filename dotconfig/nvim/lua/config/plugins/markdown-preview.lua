9578d48b059035fb6113c3093a04cd041bd6ce70
jonwin1 <jonatan.wincent@gmail.com>
2025-02-08 21:47:38 +0100

    nvim fix markdown previev update failure

--- dotconfig/nvim/lua/config/plugins/markdown-preview.lua
+++ dotconfig/nvim/lua/config/plugins/markdown-preview.lua
@@ -1,11 +1,11 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
     ft = { "markdown" },
+    build = function()
+        require("lazy").load { plugins = { "markdown-preview.nvim" } }
+        vim.fn["mkdp#util#install"]()
+    end,
 
     vim.keymap.set("n", "<leader>cp", vim.cmd.MarkdownPreviewToggle, { desc = "Preview Markdown" })
 }
