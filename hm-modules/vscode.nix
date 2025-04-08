12b9cadf3134b7098e9a6f61db4c9d6cf58c151d
jonwin1 <jonatan.wincent@gmail.com>
2025-04-08 20:21:48 +0200

    vscode: add extentions

--- hm-modules/vscode.nix
+++ hm-modules/vscode.nix
@@ -1,9 +1,14 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 {
-    programs.vscode = {
-        enable = true;
-        package = pkgs.vscodium;
-        profiles.default.extensions = with pkgs.vscode-extensions; [
-        ];
-    };
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      asvetliakov.vscode-neovim
+      jnoortheen.nix-ide
+      ms-python.python
+      llvm-vs-code-extensions.vscode-clangd
+      enkia.tokyo-night
+    ];
+  };
 }
