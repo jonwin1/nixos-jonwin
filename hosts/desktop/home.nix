2834d8424ef2df5e684296021863c3445a916b1b
jonwin1 <jonatan.wincent@gmail.com>
2025-04-21 18:42:22 +0200

    new gpu, fix monitors

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -3,20 +3,20 @@
     wayland.windowManager.hyprland = {
         settings = {
             cursor = {
-                default_monitor = "DP-3";
+                default_monitor = "DP-1";
             };
 
             monitor = [
-                "DP-3, 3440x1440@144, 0x0, 1"
-                "HDMI-A-1, 3840x2160@60, auto-left, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, vrr, 1"
+                "DP-1, 3440x1440@144, 0x0, 1"
+                "HDMI-A-1, 3840x2160@120, auto-left, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, vrr, 1"
             ];
 
             workspace = [
-                "1, monitor:DP-3, default:true"
-                "2, monitor:DP-3"
-                "3, monitor:DP-3"
-                "4, monitor:DP-3"
-                "5, monitor:DP-3"
+                "1, monitor:DP-1, default:true"
+                "2, monitor:DP-1"
+                "3, monitor:DP-1"
+                "4, monitor:DP-1"
+                "5, monitor:DP-1"
                 "6, monitor:HDMI-A-1, default:true"
                 "7, monitor:HDMI-A-1"
                 "8, monitor:HDMI-A-1"
@@ -30,7 +30,7 @@
             mainBar = {
                 "hyprland/workspaces" = {
                     persistent-workspaces = {
-                        "DP-3" = [ 1 2 3 4 5 ];
+                        "DP-1" = [ 1 2 3 4 5 ];
                         "HDMI-A-1" = [ 6 7 8 9 10 ];
                     };
                 };
