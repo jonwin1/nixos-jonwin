d22d584405450a82c24e9d98c1466b401a1e4d61
jonwin1 <jonatan.wincent@gmail.com>
2025-05-02 23:20:28 +0200

    hypridle: hm -> dot, add sleep & dimm

--- hm-modules/hypridle.nix
+++ hm-modules/hypridle.nix
@@ -1,24 +1,8 @@
 {
-    services.hypridle = {
-        enable = true;
-        settings = {
-            general = {
-                lock_cmd = "pidof hyprlock || hyprlock";
-                before_sleep_cmd = "loginctl lock-session";
-                after_sleep_cmd = "hyprctl dispatch dpms on";
-            };
-
-            listener = [
-                {
-                    timeout = 300;
-                    on-timeout = "loginctl lock-session";
-                }
-                {
-                    timeout = 330;
-                    on-timeout = "hyprctl dispatch dpms off";
-                    on-resume = "hyprctl dispatch dpms on";
-                }
-            ];
-        };
-    };
+  services.hypridle = {
+    enable = true;
+  };
+  home.file.".config/hypr/hypridle.conf" = {
+    source = ../dotconfig/hypr/hypridle.conf;
+  };
 }
