9d6db9147e3fcba1fdeac702f84e0e6b0569fb10
jonwin1 <jonatan.wincent@gmail.com>
2025-05-04 16:40:56 +0200

    hyprland: host specific settings

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -1,28 +1,8 @@
 { ... }:
 {
-  wayland.windowManager.hyprland = {
-    settings = {
-      cursor = {
-        default_monitor = "DP-1";
-      };
-
-      monitor = [
-        "DP-1, 3440x1440@144, 0x0, 1"
-        "HDMI-A-1, 3840x2160@120, auto-left, 1" # , bitdepth, 10, cm, hdr, sdrbrightness, 1.2, vrr, 1"
-      ];
-
-      workspace = [
-        "1, monitor:DP-1, default:true"
-        "2, monitor:DP-1"
-        "3, monitor:DP-1"
-        "4, monitor:DP-1"
-        "5, monitor:DP-1"
-        "6, monitor:HDMI-A-1, default:true"
-        "7, monitor:HDMI-A-1"
-        "8, monitor:HDMI-A-1"
-        "9, monitor:HDMI-A-1"
-        "10, monitor:HDMI-A-1"
-      ];
+  home.file = {
+    ".config/hypr/hyprland-host.conf" = {
+      source = ../../dotconfig/hypr/hyprland-desktop.conf;
     };
   };
 }
--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  imports = [];
+  imports = [ ];
   services = {
     batsignal = {
       enable = true;
@@ -8,21 +8,9 @@
     };
   };
 
-  wayland.windowManager.hyprland = {
-    settings = {
-      cursor = {
-        default_monitor = "eDP-1";
-      };
-
-      monitor = [ "eDP-1, highrr, auto, 1.25" ];
-
-      workspace = [
-        "1, monitor:eDP-1, default:true"
-        "2, monitor:eDP-1"
-        "3, monitor:eDP-1"
-        "4, monitor:eDP-1"
-        "5, monitor:eDP-1"
-      ];
+  home.file = {
+    ".config/hypr/hyprland-host.conf" = {
+      source = ../../dotconfig/hypr/hyprland-laptop.conf;
     };
   };
 }
