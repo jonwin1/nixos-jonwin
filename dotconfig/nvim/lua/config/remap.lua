72faa5f8dc5f95ac1cfb82231dad74c3f326ea40
jonwin1 <jonatan.wincent@gmail.com>
2025-05-05 14:54:05 +0200

    nvim: no zz on C-u & C-d

--- dotconfig/nvim/lua/config/remap.lua
+++ dotconfig/nvim/lua/config/remap.lua
@@ -10,8 +10,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Center line on scroll/search
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
