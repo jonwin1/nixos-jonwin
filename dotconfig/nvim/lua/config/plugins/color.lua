8f416aa14540b5ff756339e40319a292e0fb17ee
jonwin1 <jonatan.wincent@gmail.com>
2025-05-09 20:36:20 +0200

    prepare for transparency, more dim around special workspace

--- dotconfig/nvim/lua/config/plugins/color.lua
+++ dotconfig/nvim/lua/config/plugins/color.lua
@@ -2,8 +2,13 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+        style = "moon",
+        transparent = true,
+    },
     config = function()
         vim.cmd [[colorscheme tokyonight]]
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 }
