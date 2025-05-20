8b58f52c41721d45c5c81414cfaa6412ad101fd1
jonwin1 <jonatan.wincent@gmail.com>
2025-05-20 21:02:41 +0200

    nvim: hover simple border

--- dotconfig/nvim/lua/config/remap.lua
+++ dotconfig/nvim/lua/config/remap.lua
@@ -38,3 +38,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+
+vim.keymap.set("n", "K", function()
+	vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
+end)
