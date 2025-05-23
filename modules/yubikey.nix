709a93dd85c6f0a85a6ae6c553a5250c86ab8b4e
jonwin1 <jonatan.wincent@gmail.com>
2025-05-23 22:49:32 +0200

    lock screen when yubikey is unplugged

--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -22,4 +22,14 @@
   environment.systemPackages = with pkgs; [
     yubikey-manager
   ];
+
+  # Lock the screen when key is unplugged
+  services.udev.extraRules = ''
+    ACTION=="remove",\
+     ENV{ID_BUS}=="usb",\
+     ENV{ID_MODEL_ID}=="0407",\
+     ENV{ID_VENDOR_ID}=="1050",\
+     ENV{ID_VENDOR}=="Yubico",\
+     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
 }
