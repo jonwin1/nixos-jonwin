874ccb0fa52647d8430d79765d04bbcb4a5fb1b2
Jonatan Wincent <jonatan.wincent@proton.me>
2025-06-08 12:00:58 +0200

    move networkmanager group to networking module

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -1,7 +1,11 @@
-{ hostname, ... }:
+{ user, hostname, ... }:
 {
   networking = {
     hostName = hostname;
     networkmanager.enable = true;
   };
+
+  users.users.${user}.extraGroups = [
+    "networkmanager"
+  ];
 }
--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -8,7 +8,6 @@
       isNormalUser = true;
       extraGroups = [
         "wheel"
-        "networkmanager"
       ];
     };
   };
