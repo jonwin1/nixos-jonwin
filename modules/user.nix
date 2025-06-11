07453dc9ff83c5e83efb914641fb11b5a8c71993
Jonatan Wincent <jonatan.wincent@proton.me>
2025-06-11 16:23:40 +0200

    only autologin first tty

--- modules/user.nix
+++ modules/user.nix
@@ -11,6 +11,4 @@
       ];
     };
   };
-
-  services.getty.autologinUser = user;
 }
--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -1,27 +1,35 @@
-{ pkgs, ... }:
+{ pkgs, user, ... }:
 # https://github.com/XNM1/linux-nixos-hyprland-config-dotfiles/blob/main/nixos/yubikey.nix
+# FIXME: Don't forget to create an authorization mapping file for your user (https://wiki.nixos.org/wiki/Yubikey)
 {
-  services.udev.packages = [ pkgs.yubikey-personalization ];
-
-  programs.ssh.startAgent = true;
-
-  # FIXME: Don't forget to create an authorization mapping file for your user (https://wiki.nixos.org/wiki/Yubikey)
-  security.pam.u2f = {
-    enable = true;
-    settings.cue = true;
-    control = "sufficient";
+  services = {
+    getty.autologinUser = user;
+    getty.autologinOnce = true;
+    udev.packages = [ pkgs.yubikey-personalization ];
   };
 
-  security.pam.services = {
-    hyprlock.u2fAuth = true;
-    login.u2fAuth = true;
-    sddm.u2fAuth = true;
-    sudo.u2fAuth = true;
+  programs = {
+    ssh.startAgent = true;
   };
 
   environment.systemPackages = with pkgs; [
     yubikey-manager
   ];
+
+  security = {
+    pam = {
+      u2f = {
+        enable = true;
+        settings.cue = true;
+        control = "sufficient";
+      };
+      services = {
+        hyprlock.u2fAuth = true;
+        login.u2fAuth = true;
+        sudo.u2fAuth = true;
+      };
+    };
+  };
 
   # Lock the screen when key is unplugged
   services.udev.extraRules = ''
