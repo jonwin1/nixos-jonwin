560c8c68ce60060ed7bc5cd72258ca2d94e0e62b
Jonatan Wincent <jonatan.wincent@proton.me>
2025-08-30 21:01:11 +0200

    waybar conf

--- modules/style.nix
+++ modules/style.nix
@@ -1,7 +1,52 @@
+{ pkgs, user, ... }:
 {
   stylix = {
     enable = true;
+
     image = ../wallpaper.png;
-    polarity = "dark";
+    # polarity = "dark";
+
+    cursor = {
+      package = pkgs.rose-pine-cursor;
+      name = "BreezeX-RosePine-Linux";
+      size = 24;
+    };
+
+    fonts = {
+      # font = {
+      #   name = "FiraCode Nerd Font";
+      #   size = 12;
+      # };
+      # noto-fonts noto-fonts-emoji nerd-fonts.caskaydia-mono
+      serif = {
+        # noto serif
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        # noto sans
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        # caskaydia mono nerf font
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+
+    icons = {
+      # enable = true;
+      # package = pkgs.nordzy-icon-theme;
+      # dark = "Nordzy-dark";
+      # light = "";
+    };
   };
 }
