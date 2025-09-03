09aa37068201e0c6c5bb2d9366e5335473a449fa
Jonatan Wincent <jonatan.wincent@proton.me>
2025-09-03 22:26:41 +0200

    nvf: opts & autocmds

--- modules/nvf-nvim.nix
+++ modules/nvf-nvim.nix
@@ -1,5 +1,9 @@
 {user, ...}: {
-  home-manager.users.${user} = {inputs, ...}: {
+  home-manager.users.${user} = {
+    inputs,
+    lib,
+    ...
+  }: {
     imports = [
       inputs.nvf.homeManagerModules.default
     ];
@@ -11,9 +15,46 @@
           viAlias = true;
           vimAlias = true;
 
+          searchCase = "smart";
+
+          clipboard.enable = true;
+          undoFile.enable = true;
+
+          options = {
+            mouse = "";
+
+            tabstop = 4;
+            shiftwidth = 0;
+
+            scrolloff = 10;
+            sidescrolloff = 10;
+          };
+
+          augroups = [{name = "highlight-yank";}];
+
+          autocmds = [
+            {
+              event = ["FileType"];
+              pattern = ["tex" "markdown" "md"];
+              desc = "Auto break lines in tex and md files.";
+              command = "setlocal textwidth=80 linebreak";
+            }
+            {
+              event = ["TextYankPost"];
+              group = "highlight-yank";
+              desc = "Highlight when yanking text.";
+              callback = lib.generators.mkLuaInline ''
+                function()
+                  vim.highlight.on_yank()
+                end
+              '';
+            }
+          ];
+
           spellcheck = {
             enable = true;
             languages = ["en" "sv"];
+            programmingWordlist.enable = true;
           };
 
           lsp = {
