6d6d1bf9bf805b807630d3178e82c3b34ace656a
Jonatan Wincent <jonatan.wincent@proton.me>
2025-09-14 00:46:32 +0200

    laptop: fix unstable scaling

--- config/laptop/hyprland.nix
+++ config/laptop/hyprland.nix
@@ -6,7 +6,7 @@
       };
 
       monitor = [
-        "eDP-1, 2560x1600@120, 0x0, 1.5"
+        "eDP-1, 2560x1600@120, 0x0, 1.6"
         "desc:ASUSTek COMPUTER INC ASUS VP247 HALMTF188756, preferred, 2048x0, 1"
         "desc:AOC 2460G5 0x00011131, preferred, 3968x0, 1"
       ];
