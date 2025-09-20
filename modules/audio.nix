9d0fc21cc27f8a400e8468c356d3b361ebf2e59d
Jonatan Wincent <jonatan.wincent@proton.me>
2025-09-20 16:19:16 +0200

    fix audio crackling issue in games

--- modules/audio.nix
+++ modules/audio.nix
@@ -8,6 +8,14 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       wireplumber.enable = true;
+
+      extraConfig.pipewire-pulse."12-audio-crackling" = {
+        "pulse.properties" = {
+          "pulse.min.req" = "256/48000";
+          "pulse.min.frag" = "256/48000";
+          "pulse.min.quantum" = "256/48000";
+        };
+      };
     };
 
     playerctld.enable = true;
