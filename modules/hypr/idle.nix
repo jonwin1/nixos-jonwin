41252ff3fbd2846c89f5f172bcab249e1d1b7ca7
Jonatan Wincent <jonatan.wincent@proton.me>
2026-01-23 22:05:30 +0100

    Sleep if locked

--- modules/hypr/idle.nix
+++ modules/hypr/idle.nix
@@ -10,20 +10,24 @@
           after_sleep_cmd = "hyprctl dispatch dpms on";
         };
         listener = [
-          {
-            timeout = 300;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on && light -I";
-          }
-          {
-            timeout = 240;
+          { # Dim screen
+            timeout = 240; # 4min
             on-timeout = "light -O && light -S 10";
             on-resume = "light -I";
           }
-          {
-            timeout = 240;
+          { # Turn off keyboard backlight
+            timeout = 240; # 4min
             on-timeout = "light -s sysfs/leds/platform::kbd_backlight -O && light -s sysfs/leds/platform::kbd_backlight -S 0";
             on-resume = "light -s sysfs/leds/platform::kbd_backlight -I";
+          }
+          { # Screen off
+            timeout = 300; # 5min
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on && light -I";
+          }
+          { # Suspend if locked
+            timeout = 900; # 15min
+            on-timeout = "pidof hyprlock && systemctl suspend";
           }
         ];
       };
