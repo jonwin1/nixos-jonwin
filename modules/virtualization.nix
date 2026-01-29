7b28c9b1918d63aa2ade7bc930436135e4171bfe
Jonatan Wincent <jonatan.wincent@proton.me>
2026-01-29 16:54:02 +0100

    fix vm networking

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,9 +1,12 @@
 { pkgs, user, ... }:
 {
   programs.virt-manager.enable = true;
+
   virtualisation.libvirtd = {
     enable = true;
     qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
   };
+
   users.users.${user}.extraGroups = [ "libvirtd" ];
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
 }
