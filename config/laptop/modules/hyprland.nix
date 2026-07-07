6f6f65c9302b690fc6214f59bba0b77bea26d09f
Jonatan Wincent <jonatan.wincent@proton.me>
2026-07-07 23:33:27 +0200

    extra monitor res/pos

--- config/laptop/modules/hyprland.nix
+++ config/laptop/modules/hyprland.nix
@@ -8,8 +8,8 @@
 
       monitor = [
         "eDP-1, 2560x1600@60, 0x0, 1.6"
-        "desc:ASUSTek COMPUTER INC ASUS VP247 HALMTF188756, preferred, 2048x0, 1"
-        "desc:AOC 2460G5 0x00011131, preferred, 3968x0, 1"
+        "desc:ASUSTek COMPUTER INC ASUS VP247 HALMTF188756, 1920x1080@75, 2560x0, 1"
+        "desc:AOC 2460G5 0x00011131, 1920x1080@75, 4480x0, 1"
       ];
     };
   };
