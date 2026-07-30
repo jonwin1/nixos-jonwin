61e121863acbbbf1b9e2c21652f06a8bd89f931b
Jonatan Wincent <jonatan.wincent@proton.me>
2026-07-30 12:03:22 +0200

    stylix

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ modules/features/stylix.nix
@@ -0,0 +1,61 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.stylix = { pkgs, config, ... }: {
+    imports = [
+      self.nixosModules.user
+      inputs.stylix.nixosModules.stylix
+    ];
+
+    home-manager.users.${config.my.username}.imports = [
+      self.homeModules.stylix
+    ];
+
+    stylix = {
+      enable = true;
+
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
+
+      cursor = {
+        package = pkgs.rose-pine-cursor;
+        name = "BreezeX-RosePine-Linux";
+        size = 24;
+      };
+
+      fonts = {
+        serif = {
+          package = pkgs.noto-fonts;
+          name = "Noto Serif";
+        };
+
+        sansSerif = {
+          package = pkgs.noto-fonts;
+          name = "Noto Sans";
+        };
+
+        monospace = {
+          package = pkgs.nerd-fonts.fira-code;
+          name = "FiraCode Nerd Font";
+        };
+
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
+    };
+  };
+
+  flake.homeModules.stylix = { pkgs, ... }: {
+    stylix = {
+      targets.zen-browser.profileNames = [ "default" ];
+    };
+
+    gtk = {
+      enable = true;
+
+      iconTheme = {
+        package = pkgs.nordzy-icon-theme;
+        name = "Nordzy-dark";
+      };
+    };
+  };
+}
