cf15642f8adc43893cdc69a8967ca5d81f1056b6
Jonatan Wincent <jonatan.wincent@proton.me>
2026-07-30 11:06:21 +0200

    sddm

---
+++ modules/features/sddm.nix
@@ -0,0 +1,17 @@
+{
+  flake.nixosModules.sddm = { config, ... }: {
+    services.displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+
+      autoLogin = {
+        enable = true;
+        user = config.my.username;
+      };
+
+      defaultSession = "hyprland-uwsm"; # TODO: move to hyprland?
+    };
+  };
+}
