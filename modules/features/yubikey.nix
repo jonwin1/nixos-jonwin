5fd8488bafecf63a1e815900ab0584189a5bade6
Jonatan Wincent <jonatan.wincent@proton.me>
2026-07-30 12:06:03 +0200

    yubikey

---
+++ modules/features/yubikey.nix
@@ -0,0 +1,48 @@
+# FIXME: Don't forget to create an authorization mapping for your user (https://wiki.nixos.org/wiki/Yubikey#pam_u2f)
+{
+  flake.nixosModules.yubikey = { pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      yubioath-flutter
+    ];
+
+    services = {
+      pcscd.enable = true; # Required by yubioath-flutter.
+      udev.packages = [ pkgs.yubikey-personalization ];
+    };
+
+    security.pam = {
+      u2f = {
+        enable = true;
+        settings.cue = true;
+        control = "sufficient";
+      };
+
+      services = {
+        hyprlock = {
+          u2fAuth = true; # Enable YubiKey login.
+          unixAuth = false; # Disable password login.
+        };
+
+        login = {
+          u2fAuth = true;
+          unixAuth = false;
+        };
+
+        sudo = {
+          u2fAuth = true;
+          unixAuth = false;
+        };
+      };
+    };
+
+    # Lock the screen when key is unplugged.
+    services.udev.extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0407",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+    '';
+  };
+}
