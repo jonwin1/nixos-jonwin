761ec5f0adea80cb718617401f162afb76f80e1e
Jonatan Wincent <jonatan.wincent@proton.me>
2026-09-05 16:20:23 +0200

    mango: desktop monitors

---
+++ modules/hosts/desktop/mango.nix
@@ -0,0 +1,10 @@
+{
+  flake.wrappers.mangowc = {
+    settings = {
+      monitorrule = [
+        "make:AOC, model:U34G2G4R3, width:3440, height:1440, refresh:144, x:1920, y:0, scale:1"
+        "make:Samsung Electric Company, model:QCQ90S , width:1920, height:1080, refresh:120, x:0, y:0, scale:1"
+      ];
+    };
+  };
+}
