7cccfea584f2c6e6f86fc6e820d90e468a961e5e
Jonatan Wincent <jonatan.wincent@proton.me>
2026-09-08 11:38:08 +0200

    remove plymouth from boot

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,35 +1,14 @@
 {
-  flake.nixosModules.boot = { pkgs, ... }: {
-    boot = {
-      loader = {
-        efi.canTouchEfiVariables = true;
-        grub = {
-          enable = true;
-          efiSupport = true;
-          device = "nodev";
-          useOSProber = true;
-        };
-        timeout = 0;
-      };
-
-      # Boot splash screen
-      plymouth = {
+  flake.nixosModules.boot = {
+    boot.loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
         enable = true;
-        font = "${pkgs.nerd-fonts.fira-code}/share/fonts/truetype/NerdFonts/FiraCode/FiraCodeNerdFont-Regular.ttf";
-        logo = pkgs.fetchurl {
-          url = "https://jonwin.se/jonwin-digitag.png";
-          sha256 = "fwQTv4zO9TdtNj1SRltRAUzzNKEsHCLUrFd8OgGRwHI=";
-        };
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
       };
-
-      # Enable "Silent boot"
-      consoleLogLevel = 3;
-      initrd.verbose = false;
-      kernelParams = [
-        "quiet"
-        "rd.udev.log_level=3"
-        "rd.systemd.show_status=auto"
-      ];
+      timeout = 0;
     };
   };
 }
