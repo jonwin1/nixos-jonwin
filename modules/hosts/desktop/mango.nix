e3bd340e122ed4d7a325dd2e8ee126dba78176c6
Jonatan Wincent <jonatan.wincent@proton.me>
2026-09-23 21:49:06 +0200

    mango: 4K TV, initial focus DP-1

--- modules/hosts/desktop/mango.nix
+++ modules/hosts/desktop/mango.nix
@@ -1,9 +1,13 @@
 {
   flake.wrappers.mangowc = {
     settings = {
+      exec-once = [
+        "mmsg dispatch focusmon,DP-1"
+      ];
+
       monitorrule = [
-        "make:AOC, model:U34G2G4R3, width:3440, height:1440, refresh:144, x:1920, y:0, scale:1"
-        "make:Samsung Electric Company, model:QCQ90S, width:1920, height:1080, refresh:120, x:0, y:0, scale:1"
+        "make:AOC, model:U34G2G4R3, width:3440, height:1440, refresh:144, x:3840, y:720, scale:1"
+        "make:Samsung Electric Company, model:QCQ90S, width:3840, height:2160, refresh:144, x:0, y:0, scale:1"
       ];
     };
   };
